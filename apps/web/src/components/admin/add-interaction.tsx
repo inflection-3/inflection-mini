c257124d6afc11e41b62de4f9a0d00033f0554e4
chann44 <44chansw@gmail.com>
2025-09-15 17:28:16 +0530

    chore: type issue fixed in add interaciton

--- apps/web/src/components/admin/add-interaction.tsx
+++ apps/web/src/components/admin/add-interaction.tsx
@@ -14,6 +14,8 @@ export function AddInteractionForm() {
     verficationType: "none",
     rewardId: "",
     appId: "",
+    description: "",
+    title: ""
   });
 
   const [errors, setErrors] = useState<Partial<Record<keyof typeof formData, string>>>({});
@@ -49,6 +51,8 @@ export function AddInteractionForm() {
           verficationType: "none",
           rewardId: "",
           appId: "",
+          title: "",
+          description: ""
         });
       },
     });

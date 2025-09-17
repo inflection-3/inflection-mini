4fea5c7592a14ea6cbbd52d67e158c48e493af04
chann44 <44chansw@gmail.com>
2025-09-17 11:04:52 +0530

    featured list push

--- apps/web/src/components/featured-list.tsx
+++ apps/web/src/components/featured-list.tsx
@@ -9,20 +9,22 @@ export function FeaturedList() {
         Featured App of the day
       </h1>
       <div className="grid grid-cols-3 gap-2.5">
-      <FeaturedItem title="Geodnet" image="/test.svg" />
-      <FeaturedItem className="bg-gradient-to-r from-[#035ADE] to-[#0247CE]" title="Geodnet" image="/scan.svg" />
+      <FeaturedItem link="/apps/eebb5e2f-1144-46c8-8cf2-1b140d167691" title="Geodnet" image="/test.svg" active />
+      <FeaturedItem link="" className="bg-gradient-to-r from-[#035ADE] to-[#0247CE]" title="Geodnet" image="/scan.svg" />
       </div>
    </div>
   );
 }
 
-function FeaturedItem({ title, image, className }: {
+function FeaturedItem({ title, image, className, link, active }: {
   title: string;
   image: string;
   className?: string;
+  link: string;
+  active?: boolean;
 }) {
   return (
-    <Link to="/" className="max-w-[126px]">
+    <Link disabled={!active} to={link} className="max-w-[126px]">
       <StatCard className={cn("w-full h-[100px] flex items-center justify-center rounded-[12px]", className)}>
         <img
           className="w-[56px] h-[56px] rounded-[12px]"
